bf0815b2a4ea6df2e2e5ebc2dc454554c4a37569
Jesse Yates <jesse@fineo.io>
2016-12-22 21:15:17 -0800

    Trying to update the schema view content after loading
    its not working:
    ORIGINAL EXCEPTION: self.parentView.context.item is undefined

--- src/app/pages/schemas/components/schema/schema.component.ts
+++ src/app/pages/schemas/components/schema/schema.component.ts
@@ -12,12 +12,23 @@ import { SchemaService } from '../../../../schema.service'
 export class SchemaComponent {
 
   private schema_info:Object;
+  private added_fields:Array<Field>;
 
   constructor(private route: ActivatedRoute,
               private router: Router,
               private service: SchemaService){}
 
   ngOnInit() {
-    this.route.params.subscribe((value) => this.schema_info = this.service.getSchema(value["id"]));
+    this.route.params.subscribe(path_info => this.schema_info = this.service.getSchema(path_info["id"]))
   }
+
+  public save(){
+     // save the changes
+  }
+}
+
+class Field{
+  private name:string;
+  private aliases:Array<string>;
+  private type:string;
 }
--- src/app/schema.service.ts
+++ src/app/schema.service.ts
@@ -3,6 +3,44 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class SchemaService {
 
+  schema_infos = {
+    "1234": {
+      fields: [
+        {
+          name: "timestamp",
+          aliases: ["ts"],
+          type: "long",
+          formats: ["uuuu-MM-dd hh:mm:ss @ UTC"]
+        },
+        {
+          name: "cpu",
+          aliases: ["cpu_usage"],
+          type: "double"
+        }
+      ]
+    },
+    "5555": {
+      fields: [
+        {
+          name: "timestamp",
+          aliases: ["ts", "time"],
+          type: "long",
+          formats: ["uuuu-MM-dd hh:mm:ss @ UTC"]
+        },
+        {
+          name: "memory",
+          aliases: ["memory_usage", "mem"],
+          type: "integer"
+        },
+        {
+          name: "energy",
+          aliases: [],
+          type: "varchar"
+        }
+      ]
+    }
+  }
+
   public schemas(){
     return [{
       "id": "1234",
@@ -18,8 +56,6 @@ export class SchemaService {
 
   // Get the schema information for the given schema id (i.e. canonical name)
   public getSchema(id:string):Object{
-    return {
-      id: id
-    }
+    return this.schema_infos[id];
   }
 }
