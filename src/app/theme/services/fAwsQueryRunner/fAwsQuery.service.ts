09098ea933fe2fc59f76199d270a552f8f0fb479
Jesse Yates <jesse@fineo.io>
2017-01-16 10:56:42 -0800

    Removing fAwsQueryRunner - we have a different mechanism implemented

--- src/app/theme/services/fAwsQueryRunner/fAwsQuery.service.ts
+++
@@ -1,7 +0,0 @@
-// Definition of a query to run against an AWS service
-export class AwsQuery{
-  public region:string = 'us-east-1';
-  public service:string;
-  public body: any;
-  public path: string;
-}
--- src/app/theme/services/fAwsQueryRunner/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './fAwsQuery.service'
-export * from '.fAwsQueryRunner.service'
