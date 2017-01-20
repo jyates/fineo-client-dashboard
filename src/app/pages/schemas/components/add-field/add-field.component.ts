f1e8102bc9a239646e0b021287791c2ae9feeeda
Jesse Yates <jesse@fineo.io>
2017-01-20 13:01:17 -0800

    moving 'add' schema to using field cards, loading/saving disables + spinner for schema

--- src/app/pages/schemas/components/add-field/add-field.component.ts
+++ src/app/pages/schemas/components/add-field/add-field.component.ts
@@ -1,12 +1,18 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, EventEmitter} from '@angular/core';
 import { FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'add-field-component',
-    templateUrl: './add-field.html'
-    // styles: [require('./schema.scss')]
+    templateUrl: './add-field.html',
+    styles: [require('./add-field.scss')],
+    outputs: ['deleteItem']
 })
 export class AddFieldComponent {
     @Input() fieldForm: FormGroup; // This component is passed a FormGroup from the base component template
     @Input() index: number;
+
+    public deleteItem = new EventEmitter();
+    public handleDelete(event):void {
+      this.deleteItem.next(this.index);
+    }
 }
