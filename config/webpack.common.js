5c7ba0d4fbc4fc74494b7ae0eb87bb0e98eea8c1
Jesse Yates <jesse@fineo.io>
2017-01-23 18:54:06 -0800

    Fixing page title/description

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -23,8 +23,8 @@ const ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin');
  */
 const HMR = helpers.hasProcessFlag('hot');
 const METADATA = {
-  title: 'ng2-admin - Angular 2 Admin Template',
-  description: 'Free Angular 2 and Bootstrap 4 Admin Template',
+  title: 'Fineo Dashboard',
+  description: 'Dashboard for manually manipulating Fineo APIs',
   baseUrl: '/',
   isDevServer: helpers.isWebpackDevServer()
 };
