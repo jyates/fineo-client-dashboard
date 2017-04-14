c942a86e4d0bbcf43b48d668b0099c856a885b34
Jesse Yates <jesse@fineo.io>
2017-04-13 19:25:33 -0700

    Fixing last dashboard index.ts error

--- src/app/pages/dashboard/components/index.ts
+++ src/app/pages/dashboard/components/index.ts
@@ -1,6 +1,6 @@
 export * from './query';
 export * from './item.config';
 export * from './base.card.component';
-export * from './base.data.item.component';
+export * from './base.data.item';
 export * from './dashboard.card';
 export * from './card.config';
